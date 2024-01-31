ea9e5f1ab0cd3af2382e8c9439eb84f0b4d11e26
Thomas Alvarenga <thomas@bloq.com>
2024-01-31 10:36:03 -0300

    Fixed layout to be responsive

--- apps/web/src/presentation/components/Hero.tsx
+++ apps/web/src/presentation/components/Hero.tsx
@@ -3,9 +3,10 @@ import logo from '../../assets/logo.svg'
 
 function Hero(): JSX.Element {
   return (
-    <div className="flex flex-col justify-center items-center h-36">
+    <div className="flex flex-col justify-center items-center h-24">
       <img src={logo} className="" alt="logo" />
-      <h2 className="font-poller text-white text-3xl mt-8">
+      <h2 className="font-poller text-white text-center text-xs mt-4
+        sm:text-lg md:text-xl lg:text-3xl">
         Learn how Ethereum & Bitcoin Finality Works.
       </h2>
     </div>
