ca10b62577d980774a39ad0a3ae7439341bffd53
Thomas Alvarenga <thomas@bloq.com>
2024-09-19 13:50:29 -0300

    Added sold out label to earth Hatchling element egg

--- apps/web/src/presentation/components/stages/Eggs.tsx
+++ apps/web/src/presentation/components/stages/Eggs.tsx
@@ -8,12 +8,46 @@ const earthEgg = getImage('earth-egg.png')
 import chevron from '../../../assets/chevron.svg'
 import { ElementEnum } from '@hemihatchlings/shared'
 
+interface EggProps {
+  index: number
+  soldOut: boolean
+  image: string
+  onClick: () => void
+}
+
+const Egg = ({ index, soldOut, image, onClick }: EggProps) => (<>
+  { soldOut ?
+    <div className="border border-white transition duration-500 hover:scale-110 relative">
+      <label className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
+        z-10 font-monaco text-xl text-white md:text-2xl xl:text-3xl">
+        SOLD OUT
+      </label>
+      <img className="blur" src={image} alt="Fire Egg" />
+    </div>
+  : 
+    <button onClick={onClick}>
+      <div className="border border-white transition duration-500 hover:scale-110">
+        <img src={image} alt="Fire Egg" />
+      </div>
+    </button>
+  }
+  <div className="font-monaco text-sm text-white mt-2 text-left
+    sm:text-base md:text-lg md:mt-4">
+    {index.toString().padStart(2, '0')}
+  </div>
+</>)
+
 interface EggsProps {
   hatchEgg: (element: ElementEnum) => void
 }
 
 function Eggs({ hatchEgg }:EggsProps): JSX.Element {
   const [active, setActive] = useState(0)
+  const soldOut = {
+    fire:  import.meta.env.VITE_FIRE_SOLD_OUT === 'true',
+    water: import.meta.env.VITE_WATER_SOLD_OUT === 'true',
+    earth: import.meta.env.VITE_EARTH_SOLD_OUT === 'true'
+  }
 
   const goPrevious = () => {
     setActive(active - 1)
@@ -34,39 +68,15 @@ function Eggs({ hatchEgg }:EggsProps): JSX.Element {
       </button>
 
       <div className={active === 0 ? '' : 'hidden'}>
-        <button onClick={() => hatchEgg(ElementEnum.Fire)}>
-          <div className="border border-white transition duration-500 hover:scale-110">
-            <img src={fireEgg} alt="Fire Egg" />
-          </div>
-          <div className="font-monaco text-sm text-white mt-2 text-left
-            sm:text-base md:text-lg md:mt-4">
-            01
-          </div>
-        </button>
+        <Egg index={1} soldOut={soldOut.fire} image={fireEgg} onClick={() => hatchEgg(ElementEnum.Fire)} />
       </div>
 
       <div className={`${active === 1 ? '' : 'hidden'} lg:block`}>
-        <button onClick={() => hatchEgg(ElementEnum.Water)}>
-          <div className="border border-white transition duration-500 hover:scale-110">
-            <img src={waterEgg} alt="Water Egg" />
-          </div>
-          <div className="font-monaco text-sm text-white mt-2 text-left
-            sm:text-base md:text-lg md:mt-4">
-            02
-          </div>
-        </button>
+        <Egg index={2} soldOut={soldOut.water} image={waterEgg} onClick={() => hatchEgg(ElementEnum.Water)} />
       </div>
 
       <div className={`${active === 2 ? '' : 'hidden'} lg:block`}>
-        <button onClick={() => hatchEgg(ElementEnum.Earth)}>
-          <div className="border border-white transition duration-500 hover:scale-110">
-            <img src={earthEgg} alt="Earth Egg" />
-          </div>
-          <div className="font-monaco text-sm text-white mt-2 text-left
-            sm:text-base md:text-lg md:mt-4">
-            03
-          </div>
-        </button>
+        <Egg index={3} soldOut={soldOut.earth} image={earthEgg} onClick={() => hatchEgg(ElementEnum.Earth)} />
       </div>
 
       <button
